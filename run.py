74ee83530daa7b880f3dc45970f993b611934838
Haashim Chadhary <haashim379@outlook.com>
2024-07-04 23:13:31 +0000

    feat: API access created and connected to google spreadsheet, reading the sheets data

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SHEET_NAME = "Deploma3"
+SHEET_NAME_SUBSHEET_1 = "Profile"
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open(SHEET_NAME)
+
+profiles = SHEET.worksheet(SHEET_NAME_SUBSHEET_1)
+
+data = profiles.get_all_values()
+
+print(data)
