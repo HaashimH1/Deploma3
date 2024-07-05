74140a6a483f086a6dbaf8f1b53c1982f73daf68
Haashim Chadhary <haashim379@outlook.com>
2024-07-05 00:19:46 +0000

    feat: moved google sheet handling to another file in a class

---
+++ sheets.py
@@ -0,0 +1,35 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Global var for custom data to access google sheet as may be updated
+
+SPREADSHEET_NAME = "Deploma3"
+SHEET_NAME = "Profile"
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+class GoogleSheets:
+
+    # setup API to google sheet 
+    def __init__(self):
+        self.CREDS = Credentials.from_service_account_file("creds.json")
+        self.SCOPED_CREDS = self.CREDS.with_scopes(SCOPE)
+        self.GSPREAD_CLIENT = gspread.authorize(self.SCOPED_CREDS)
+        self.SHEET = self.GSPREAD_CLIENT.open(SPREADSHEET_NAME)
+        self.profiles = self.SHEET.worksheet(SHEET_NAME)
+
+    
+    # retrieves the data from the sheet
+    def get_data(self):
+        data = self.profiles.get_all_values()
+        return data
+
+        
+
+        
+
+
