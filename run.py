83d7f2ead075ff3b27ccb421caa0a07d11eb5111
Haashim Chadhary <haashim379@outlook.com>
2024-07-07 20:40:32 +0000

    feat: function to display data as a table to console

--- run.py
+++ run.py
@@ -22,8 +22,52 @@ def main():
     valid = Validator()
     gsheet = GoogleSheets()
 
-    print(gsheet.get_all_data())
-    gsheet.delete_row_at_id(3)
+    print_table(gsheet.get_all_data())
+
+
+
+def print_table(data):
+    # Calculate the maximum length for firstname, lastname, and email columns
+    max_firstname_len = max(len(row[1]) for row in data)
+    max_lastname_len = max(len(row[2]) for row in data)
+    max_email_len = max(len(row[4]) for row in data)
+
+    # Calculate the total length for the border dashes
+    border_dashes_count = 60 + max_firstname_len + max_lastname_len + max_email_len 
+    dashes = "━" * border_dashes_count
+
+    # Create the table with the top border
+    table = f"{dashes}\n"
+
+    # Format each row and add it to the table
+    for i, row in enumerate(data):
+        table += "│"
+        table += f"{row[0].upper():2}│"
+        table += f"{row[1].upper():<{max_firstname_len}}│"
+        table += f"{row[2].upper():<{max_lastname_len}}│"
+        table += f"{row[3].upper():10}│"
+        table += f"{row[4].upper():<{max_email_len}}│"
+        table += f"{row[5].upper():12}│"
+        table += f"{row[6].upper():10}│"
+        table += f"{row[7].upper():6}│"
+        table += f"{row[8].upper():10}│"
+        table += "\n"
+
+        # adds border below header row
+        if i == 0:
+            table += f"{dashes}\n"
+
+    # Add the bottom border
+    table += dashes
+
+    
+
+    # Print the table
+    print(table)
+            
+
+
+
     
 
 
